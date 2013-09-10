d405924ec093824d8027a4bbe28dfef01c5e0967
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:22:29 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,13 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX_SuggestionProvider.js',
+	'XXX_SuggestionProviderHelpers.js',
+	'XXX_CombinedSuggestionProvider.js',
+	'XXX_SuggestionOptionSelection.js',
+	'XXX_SuggestionController.js'
+),
+'initialize.js');
+
+?>
